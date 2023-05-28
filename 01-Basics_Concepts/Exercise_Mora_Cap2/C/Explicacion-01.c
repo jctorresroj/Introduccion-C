d563fa8998701a6152f591b38b6e0e7e19dfd537
jctorresroj <jctorresroj@unal.edu.co>
2023-05-27 22:34:35 -0500

    Se reviso como es el proceso de ejecutar y compilar dado el caso en todos los lenguajes

---
+++ 01-Basics_Concepts/Exercise_Mora_Cap2/C/Explicacion-01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+// se utiliza la biblioteca Standard Input Output
+
+int main(){
+  // Siempre tiene que estar la funcion main como la funcion principal
+  // Dentro de los parentesis despues de main no hay nada, luego no tiene parametros o argumentos
+  // La palabra INT indica que debe devolver un valor entero
+
+  printf("Este es mi primer programa . \n");
+  // printf es una fucion propia de c(incluida en el encabezado)y sirve para mostar algun resultado.
+  // el \n sirve para crrear una nueva linea dentro del programa
+  return 0;
+  // Retorna el valor de 0
+}
---
+++ 01-Basics_Concepts/Exercise_Mora_Cap2/CPP/Explicacion-01.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+// Libreria estandar para el input and Output Stream
+// std:: hace referenca a la libreri estandar
+
+int main(){
+  std::cout <<"Este es mi primer programa en C++" << std::endl;
+  return 0;
+}
